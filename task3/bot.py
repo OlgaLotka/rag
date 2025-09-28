3e63e84743a2fece6f187b3259304852fd2ceed5
Olga Lotka <Lotka_a@mail.ru>
2025-09-28 15:57:36 +0300

    fix bot

--- task3/bot.py
+++ task3/bot.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import os, random, re
+import os, random, re, datetime
 from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -7,16 +7,11 @@ from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.prompts import PromptTemplate
-from langchain_core.messages import HumanMessage
 import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
 from langchain_huggingface import HuggingFacePipeline
-
-#app = Flask(__name__)
-
-#model_name = "sentence-transformers/all-mpnet-base-v2"
 
 
 model_name = "mixedbread-ai/mxbai-embed-large-v1"
@@ -26,24 +21,43 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, filename="bot_log.log",filemode="w", format="%(asctime)s %(levelname)s %(message)s")
+#logging.basicConfig(level=logging.INFO, filename="bot_log.log",filemode="w", format="%(asctime)s %(levelname)s %(message)s")
 
 def generate_emb(name):
+    logger.info(f"Generate Embeddings")
     splitter = RecursiveCharacterTextSplitter(chunk_size = 400, chunk_overlap = 50)
+    
     list = os.listdir(name)
     for el in list:
         with open(name+'\\'+el, 'r') as input_file:
             data = input_file.read()
-    
+            logger.info(f"Split file")
             chunks = splitter.split_text(data)
             meta = []
             for ch in chunks:
+                logger.info(f"add meta")
                 meta.append({"name":name,"url": el, "id": ch.index})
             save(chunks, meta)
 
 def save(chunks, meta):
     vector_store = FAISS.from_texts(chunks, model, meta)
+    logger.info(f"index updated at {datetime.datetime.now}, {len(chunks)} files added, 0 errors")
     vector_store.save_local("local")
+
+def getMetadata(docs):
+    comb_text = ""
+    for doc in docs:
+        for k, v in doc.metadata.items():
+            if k == "url":
+                comb_text += "<a href=" +v+">"
+    return comb_text
+
+def process_llm_response(llm_response):  
+    # Заменить символы новой строки с пользовательскими заполнителями  
+    index = llm_response["result"].find("Answer:")
+    processed_response = llm_response["result"][index::].replace("\n", "")
+    # Отправить обработанный ответ  
+    return processed_response.join(getMetadata(llm_response["source_documents"]))  
 
 def simple_question(question: str)->bool:
     array = [
@@ -53,48 +67,46 @@ def simple_question(question: str)->bool:
     ]
     return any(re.search(p, question, re.IGNORECASE) for p in array)
 
-#@app.route('/query', methods=['GET'])
 async def find(update: Update, context: CallbackContext):
 
-    #query = request.args.get('text')
-    query = update.message.text
+    query1 = update.message.text
     user_id = update.effective_user.id
-    logger.info(f"new question {query} from {user_id}")
-    if simple_question(query):
+    logger.info(f"new question {query1} from user {user_id}")
+    if simple_question(query1):
         res = random.choice(["The problem is choice", "Can I help you?", "Let me tell about something"])
         await update.message.reply_text(res)
     else:
         wait_anw = await update.message.reply_text("Wait a minute! Thinking...")
         await context.bot.send_chat_action(action = "typing", chat_id = update.message.chat_id)
         vector_store = FAISS.load_local("local",model,allow_dangerous_deserialization=True)
-        ret = vector_store.as_retriever(k=3, lambda_mult= 0.5)  
-        docs = ret.invoke(query)
-
+        ret = vector_store.as_retriever(k=2, lambda_mult= 0.7)  
+        #docs = ret.invoke(query1)
+        #logger.info(f"Doc count {len(docs)} for user {user_id}")
         #comb_text = "\n".join(doc.page_content for doc in docs) 
 
         llm = HuggingFacePipeline.from_model_id(
             model_id="gpt2",
             task="text-generation"
         )
-        prompt_template = ChatPromptTemplate([
-            ("system", "You are a helpful assistant"),
-            MessagesPlaceholder("query")
-        ])
-
+        template = "System: {system3}\nContext: {context}\nQuestion: {question}\nAnswer:"
     #  prompt = prompt_template.format_prompt(query= [HumanMessage(content=query)]).to_string
-        QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context"], template=" Context: {context} User")
+        QA_CHAIN_PROMPT = PromptTemplate(input_variables=["system3", "context", "question"], template=template, validate_template=True)
+        formatted_prompt = QA_CHAIN_PROMPT.format(system3="You are a assistant ", context="Provide a short answer. Do not return context in answer", question=query1)
 
-        
         qa_chain = RetrievalQA.from_llm(
         llm=llm,
         verbose=True,
         retriever=ret,
-        prompt = QA_CHAIN_PROMPT
-        #type = 
+        #prompt = QA_CHAIN_PROMPT,
+        #chain_type="stuff",
+        return_source_documents=True
         #chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
         )
-        res = qa_chain.invoke({"System": "You are a helpful assistant ","context": "Only name return", "query": query})
-        await update.message.reply_text(res["result"], parse_mode="HTML")
+        res = qa_chain.invoke({"query":formatted_prompt}) #qa_chain.invoke({"system":"You are a helpful assistant ", "context":"Provide full answer", "question":query1, "query":query1})
+       
+        logger.info(f"Response {res} for user {user_id}")
+        answer =process_llm_response(res)[::2000]
+        await update.message.reply_text(answer, parse_mode="HTML")
 
 async def start(update: Update, context: CallbackContext):
     await update.message.reply_text("Hello, My name is The Oracle")
@@ -123,4 +135,3 @@ if __name__ == '__main__':
         main()
     except Exception as e:
         logger.error(e)
-    #app.run(host="0.0.0.0", port=8082, debug=True)
